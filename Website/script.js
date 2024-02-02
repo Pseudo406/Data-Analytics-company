669ea70bd2d3bb6ccfdc4aedb9d56722dbe44d74
fahimshkk <fahimshaikh3115@gmail.com>
2024-02-02 12:33:48 -0800

    Updated Cursor

--- Website/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $(".sidebar-service-list a").on("click", function(e) {
-        e.preventDefault();
-        var targetContentId = $(this).attr("href");
-        $(".service-content").hide();
-        $(targetContentId).show();
-    });
-});
